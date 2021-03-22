9b90797a7a1ccbe7de4c4f86885fffd0d5a0bf4c
Dibu Matei <matdibu@protonmail.com>
2021-03-23 01:08:59 +0200

    fixed tests and code

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -21,35 +21,78 @@ public class ServiceTest {
     @Test
     public void test_addStudent_success() {
         String id = "1";
-        int result = service.saveStudent(id, "tester", 1);
-        assertEquals(result,1);
+        int result = service.saveStudent(id, "tester", 933);
+        assertEquals(1,result);
     }
 
     @Test
-    public void test_addStudent_negativeGroup() {
+    public void test_addStudent_groupBelowRange() {
         String id = "1";
-        int result = service.saveStudent(id, "tester", -1);
-        assertEquals(result,0);
+        int result = service.saveStudent(id, "tester", 100);
+        assertEquals(0,result);
     }
 
     @Test
-    public void test_addStudent_emptyId() {
+    public void test_addStudent_groupAboveRange() {
+        String id = "1";
+        int result = service.saveStudent(id, "tester", 999);
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void test_addStudent_idEmptyString() {
         String id = "";
-        int result = service.saveStudent(id, "tester", 1);
-        assertEquals(result,0);
+        int result = service.saveStudent(id, "tester", 933);
+        assertEquals(0,result);
     }
 
     @Test
-    public void test_addStudent_emptyName() {
+    public void test_addStudent_nameEmptyString() {
         String id = "1";
-        int result = service.saveStudent(id, "", 1);
-        assertEquals(result,0);
+        int result = service.saveStudent(id, "", 933);
+        assertEquals(0,result);
     }
 
     @Test
-    public void test_addStudent_zeroGroup() {
+    public void test_addStudent_belowLowerBoundary() {
         String id = "1";
-        int result = service.saveStudent(id, "tester", 0);
-        assertEquals(result,0);
+        int result = service.saveStudent(id, "tester", 109);
+        assertEquals(0,result);
     }
+
+    @Test
+    public void test_addStudent_onLowerBoundary() {
+        String id = "1";
+        int result = service.saveStudent(id, "tester", 110);
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void test_addStudent_lowerBoundaryLimit() {
+        String id = "1";
+        int result = service.saveStudent(id, "tester", 111);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void test_addStudent_upperBoundaryLimit() {
+        String id = "1";
+        int result = service.saveStudent(id, "tester", 937);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void test_addStudent_onUpperBoundary() {
+        String id = "1";
+        int result = service.saveStudent(id, "tester", 938);
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void test_addStudent_aboveUpperBoundary() {
+        String id = "1";
+        int result = service.saveStudent(id, "tester", 939);
+        assertEquals(0,result);
+    }
+
 }
